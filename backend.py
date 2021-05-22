1700b9a15a79c3eb22637fe96cdc81d8e7233bf2
ViniPolaca <vipolachini@uol.com.br>
2021-05-21 21:24:55 -0300

    Rotas

--- backend.py
+++ backend.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("Paginas/index.html")
+def index():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/user', methods = ['POST', 'GET'])
+def user():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("user.html", result = result)
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
